f030cd04f129d80eed5ed16074dd0ea5fc6e05ff
Drlazinho <lazbonfim@hotmail.com>
2022-10-17 15:15:11 -0300

    commit: Resgatando dados do banco

--- src/app/components/messages/messages.component.ts
+++ src/app/components/messages/messages.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { MessageService } from "src/app/services/message.service";
-
-
 
 @Component({
   selector: 'app-messages',
@@ -11,8 +8,6 @@ import { MessageService } from "src/app/services/message.service";
 })
 
 export class MessagesComponent {
-
-  faTimes = faTimes;
 
   constructor(public messagesService : MessageService){
   }
